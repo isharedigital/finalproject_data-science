53ebb94a88393d206e6a515040398966e7663462
Son Nguyen <sonnguyen@Sons-MacBook-Air.local>
2024-07-15 13:48:33 +0700

    clean_remove: create func remove white space

---
+++ scripts/data_cleaning.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Xử lý khoảng trắng
+def remove_whitespace(df):
+    """
+    Loại bỏ khoảng cách ở đầu và cuối của tất cả các giá trị trong các cột loại object của DataFrame.
+    """
+    object_columns = df.select_dtypes(include=['object']).columns
+    df[object_columns] = df[object_columns].apply(lambda x: x.str.strip())
+    return df
